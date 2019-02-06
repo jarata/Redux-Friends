ea9127e6964dc1a7d810f221fcbb770f3d3553c0
J A <jarata@gmail.com>
2019-02-06 14:49:19 -0800

    building reducers

---
+++ friends/src/reducers/friendsReducer.js
@@ -0,0 +1,19 @@
+import "../actions"
+
+const initalState = {
+    fetchingFriends: false,
+    friendsFetched: false,
+    friendsSaved: false,
+    savingFriends: false,
+    updatingFriend: false,
+    friendUpdated: false,
+    deletingFriend: false,
+    friendDeleted: false,
+    friends: [],
+    error: null
+};
+export const freindReducer = () => {
+    switch () {
+        
+    }
+}
