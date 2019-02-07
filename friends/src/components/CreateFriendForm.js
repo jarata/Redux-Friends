1717a5ca716f50333bdc91dc3a2b19de202c373d
J A <jarata@gmail.com>
2019-02-07 13:37:00 -0800

    built Friend

---
+++ friends/src/components/CreateFriendForm.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class CreateFriendForm extends Component {
+    render() {
+        return (
+            <div>
+                test create
+            </div>
+        );
+    }
+}
+
+CreateFriendForm.propTypes = {};
+
+export default CreateFriendForm;
