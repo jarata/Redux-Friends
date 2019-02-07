7fa9d586bd9a6c0fc671476cc4b8ac08bca226b7
J A <jarata@gmail.com>
2019-02-07 15:31:21 -0800

    fixing App and Index

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Friend from './components/Friend'
-import CreateFriendForm from './components/CreateFriendForm'
-import UpdateFriendForm from './components/UpdateFriendForm'
+
+import FriendsContainer from './components/FriendsContainer'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Friend />
-        <CreateFriendForm />
-        <UpdateFriendForm />
+        <FriendsContainer />
       </div>
     );
   }
