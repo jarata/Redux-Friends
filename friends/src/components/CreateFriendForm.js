7a9b1080a6529526b9fec637b15ae049815e8e21
J A <jarata@gmail.com>
2019-02-07 18:45:12 -0800

    building CreateFriendForm

--- friends/src/components/CreateFriendForm.js
+++ friends/src/components/CreateFriendForm.js
@@ -1,13 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class CreateFriendForm extends Component {
-    render() {
-        return (
-            <div>
-                test create
-            </div>
-        );
-    }
+const CreateFriendForm = props => {
+    return (
+        <div>
+            <form>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter name here"
+                    onChange={}
+                    value={}
+                />
+                <input
+                    type="text"
+                    name="age"
+                    placeholder="Enter age here"
+                    onChange={}
+                    value={}
+                />
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Enter eMail here"
+                    onChange={}
+                    value={}
+                />
+                <button onClick={}>Add New Friend!</button>
+            </form>
+        </div>
+    )
 }
-
 export default CreateFriendForm;
